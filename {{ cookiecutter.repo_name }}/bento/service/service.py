d951892d8ed45c01ef92f1192b4f586b0f465ac9
prochet <pierre-rochet@outlook.com>
2022-12-15 09:45:55 +0100

    clean import

--- {{ cookiecutter.repo_name }}/bento/service/service.py
+++ {{ cookiecutter.repo_name }}/bento/service/service.py
@@ -4,7 +4,6 @@ import bentoml
 from bentoml.io import JSON, NumpyNdarray
 from numpy.typing import NDArray
 
-
 runner = bentoml.sklearn.get("pmb-model:latest").to_runner()
 
 svc = bentoml.Service("pmb-model", runners=[runner])
